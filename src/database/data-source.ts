c2877385c3cf80eae507b75236459149b4b39f12
willian piccinin <picci1522@gmail.com>
2026-09-11 14:40:54 +0000

    feat: cria entidade de usuário

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,13 +1,14 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import { User } from '../entities/User';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   synchronize: true,
   logging: false,
-  entities: [],
+  entities: [User],
   migrations: [],
   subscribers: [],
   ssl: {
